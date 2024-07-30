9299d12699afcda2b22dcbfda19866042e9f8dc4
lys <lys@ketiict.com>
2024-07-30 14:45:42 +0900

    Write comments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI() # FastAPI 인스턴스 생성
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/")   # app에 Get 요청을 받았을 때 아래 함수 실행
+async def root():   # 비동기 함수
+    return {"message": "Hello World"}   # JSON 형태의 응답 반환
