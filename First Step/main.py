3b11e8d1975357266b37835fec67cda1466a92ba
lys <lys@ketiict.com>
2024-07-31 09:57:41 +0900

    Advanced version

--- First Step/main.py
+++ First Step/main.py
@@ -1,5 +1,15 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+
+class User(BaseModel):
+    age: int
+    name: str
+    lys: Optional[bool] = None
+
+
 
 app = FastAPI()
 
@@ -10,3 +20,7 @@ def read_root():
 @app.get("/user/{user_id}")
 def read_item(user_id: int, name: Optional[str] = None):
     return {"user_id": user_id, "name": name}
+
+@app.put("/users/{user_id}")
+def update_user(user_id: int, user: User):
+    return {"user_name": user.name, "user_id": user_id}
